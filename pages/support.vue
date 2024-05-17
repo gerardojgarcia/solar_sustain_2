87ca6c1ebcfe3a88221cb29fa93055b7ab7680cc
Gerardo Garcia <gerardojgarcia@pm.me>
2024-05-16 22:25:52 -0700

    support page updates

--- pages/support.vue
+++ pages/support.vue
@@ -7,7 +7,51 @@
 
         <div class="py-32 dark:text-white">
             <h1 class="text-5xl">Customer Service & Support</h1>
-            <div class="terms-box container m-auto  space-y-8"></div>
+            <div class="terms-box container m-auto  space-y-8">
+                <p>Your satisfaction and the optimal performance of your solar panels are our top priorities at SolarSustain. Whether you need help scheduling a service, have questions about our maintenance plans, or require technical support, our dedicated team is here to assist you every step of the way.</p>
+
+                <h2 class="text-4xl">Get in touch at your convenience</h2>
+                <!--Suppport info-->
+                <div class="flex flex-col">
+                    <strong class="text-4xl">Customer Service Hotline</strong>
+                    <strong class="text-2xl">Immediate Assistance When You Need It</strong>
+                    <p class="text-2xl">Reach out to our friendly customer service team for quick answers and efficient solutions. Available from 8 AM to 6 PM, Monday through Friday.</p>
+                </div>
+                 <!--Suppport info-->
+                <div class="flex flex-col">
+                    <strong class="text-3xl">Email Support</strong>
+                    <strong>Convenient & Comprehensive Help</strong>
+                    <p>Reach out to our friendly customer service team for quick answers and efficient solutions. Available from 8 AM to 6 PM, Monday through Friday.</p>
+                    <p>    Email: support@solarsustain.com</p>
+                </div>
+                 <!--Suppport info-->
+                <div class="flex flex-col">
+                    <strong class="text-3xl">Online Help Center</strong>
+                    <strong>Self-Service Resources</strong>
+                    <p>Reach out to our friendly customer service team for quick answers and efficient solutions. Available from 8 AM to 6 PM, Monday through Friday.</p>
+                </div>
+               
+                <!--Suppport info-->
+                <div class="flex flex-col">
+                    <strong class="text-3xl">Scheduled Callbacks</strong>
+                    <strong>Talk at your convenience</strong>
+                    <p>Busy schedule? Request a callback at a time that suits you best, and our support team will contact you.</p>
+                </div>
+                <!--Suppport info-->
+                <div class="flex flex-col">
+                    <strong class="text-3xl">Service Request Form:</strong>
+                    <strong>Easy Online Booking for Service and Maintenance</strong>
+                    <p>If you need to schedule a service or maintenance check, fill out our simple online form, and we will set up everything according to your convenience.</p>
+                </div>
+                <!--Suppport info-->
+                <div class="flex flex-col">
+                    <strong class="text-3xl">Feedback & Suggestions:</strong>
+                    <strong>We Value Your Input</strong>
+                    <p>Your feedback helps us improve and serve you better. Please let us know how we can enhance our services or your experience with SolarSustain.</p>
+                </div>
+
+                    <p class="support-summary text-2xl font-bold">At SolarSustain, we're committed to providing you with exceptional support and service. Don't hesitate to reach out with any questions or needs; we're here to ensure your solar panels are always in top condition</p>
+            </div>
         </div>
 
         <TheFooter/>
