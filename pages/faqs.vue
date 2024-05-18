146bc0657598185d2e4166e1582159ca0587c900
Gerardo Garcia <gerardojgarcia@pm.me>
2024-05-18 11:07:58 -0700

    added copy to faqs page

--- pages/faqs.vue
+++ pages/faqs.vue
@@ -8,8 +8,35 @@
 
         <div class="p-32 dar:text-white">
             <h1 class="text-5xl">FAQs</h1>
-        </div>
+            <p class="intro">Have questions about our solar panel maintenance services? We've got answers! Below you'll find the most frequently asked questions from our clients. If you don't find what you're looking for, feel free to contact us directly.</p>
 
+            <div class="faq-grid grid grid-cols-1 md:grid-cols-2 gap-8">
+                <div class="faq">
+                    <h2 class="text-3xl">How often should I have my solar panels cleaned?</h2>
+                    <p class="text-lg">We recommend having your solar panels cleaned at least once a year. This will help to ensure that your panels are operating at peak efficiency and producing as much energy as possible.</p>
+                </div>
+                <div class="faq">
+                    <h2 class="text-3xl">How much does it cost to have my solar panels cleaned?</h2>
+                    <p class="text-lg">The cost of having your solar panels cleaned will depend on a number of factors, including the size of your system and the condition of your panels. Contact us today for a free quote!</p>
+                </div>
+                <div class="faq">
+                    <h2 class="text-3xl">Do I need to be home when you clean my solar panels?</h2>
+                    <p class="text-lg">No, you do not need to be home when we clean your solar panels. We will take care of everything for you, and you can rest assured that your panels will be cleaned to the highest standard.</p>
+                </div>
+                <div class="faq">
+                    <h2 class="text-3xl">How long does it take to clean my solar panels?</h2>
+                    <p class="text-lg">The time it takes to clean your solar panels will depend on the size of your system and the condition of your panels. In most cases, we can complete the job in just a few hours.</p>
+                </div>
+                <div class="faq">
+                    <h2 class="text-3xl">Do you offer any other solar panel maintenance services?</h2>
+                    <p class="text-lg">Yes, we offer a range of solar panel maintenance services to help keep your system running smoothly. Contact us today to learn more about our services and how we can help you.</p>
+                </div>
+                <div class="faq">
+                    <h2 class="text-3xl">What sets you apart from other solar panel maintenance companies?</h2>
+                    <p class="text-lg">At Solar Sustain, we are committed to providing the highest level of service to our clients. We  use the latest technology and techniques to ensure that your solar panels are always operating at peak efficiency.</p>
+                    </div>
+        </div>
+</div>
         <TheFooter/>
     </div>
 </template>
