51bb1230d12bbe57f97e1a4d86c58e2c8a85eb4f
Gerardo J Garcia <gerardojgarcia@pm.me>
2024-05-19 23:17:58 -0700

    updated process section; updated hero button text-color

--- components/TheHero.vue
+++ components/TheHero.vue
@@ -1,26 +1,38 @@
-<script setup>
-
-</script>
+<script setup></script>
 <template>
-    <div class="hero grid place-items-center py-32  h-screen" >
-        <div class="heading w-1/2 flex flex-col items-center bg-opacity-85 rounded-lg py-6 bg-white ">
-            <h1 class="text-5xl text-center m-auto mb-8 px-12 dark:text-black ">Shine Brighter with <strong class="text-yellow-400">Premium Solar Care</strong></h1>
-            <p class="subheading text-center text-2xl px-32 dark:text-black">Extend the life and efficiency of your solar investment with expert maintenance and real-time support</p>
-        </div>
-        <div class="button-container bg-white bg-opacity-85 px-4 rounded-lg justify-between py-4 mt-16 flex w-1/4">
-        <NuxtLink to="support" class="hero-quote bg-blue-400 hover:bg-blue-200 hover:text-black p-2 rounded-md">Get A Free Quote</NuxtLink>
-        <NuxtLink to="#services" class="hero-quote p-2 border-2 border-gray-400 rounded-md hover:bg-yellow-300 hover:text-black hover:border-yellow-300" >Explore Our Services</NuxtLink>
-        
-        </div>
+  <div class="hero grid place-items-center py-32 h-screen">
+    <div
+      class="heading w-1/2 flex flex-col items-center bg-opacity-85 rounded-lg py-6 bg-white"
+    >
+      <h1 class="text-5xl text-center m-auto mb-8 px-12 dark:text-black">
+        Shine Brighter with <strong class="text-yellow-400">Premium Solar Care</strong>
+      </h1>
+      <p class="subheading text-center text-2xl px-32 dark:text-black">
+        Extend the life and efficiency of your solar investment with expert maintenance
+        and real-time support
+      </p>
     </div>
+    <div
+      class="button-container bg-white bg-opacity-85 px-4 rounded-lg justify-between py-4 mt-16 flex w-1/4"
+    >
+      <NuxtLink
+        to="support"
+        class="hero-quote bg-blue-400 hover:bg-blue-300 hover:text-black p-2 rounded-md duration-150"
+        >Get A Free Quote</NuxtLink
+      >
+      <NuxtLink
+        to="#services"
+        class="hero-quote p-2 border-2 border-gray-400 rounded-md hover:bg-yellow-300 text-black hover:border-yellow-300 duration-150"
+        >Explore Our Services</NuxtLink
+      >
+    </div>
+  </div>
 </template>
 <style scoped>
-
 .hero {
-    background-image: url('/solar_sustain_5.webp');
-    background-repeat: no-repeat;
-    background-position: top;
-    background-size: cover;
+  background-image: url("/solar_sustain_5.webp");
+  background-repeat: no-repeat;
+  background-position: top;
+  background-size: cover;
 }
-
 </style>
--- components/TheProcess.vue
+++ components/TheProcess.vue
@@ -1,46 +1,91 @@
-<script setup>
-
-
-</script>
+<script setup></script>
 
 <template>
-    <section id="process" class="dark:bg-black pt-16 pb-24 overflow-hidden">
-
-<div class="process-container  space-y-16 ">
-<!-- Process Card -->
-<div class="process-card grid grid-cols-2 p-32 items-center space-y-6  " data-aos="fade-up" data-aos-duration="1500" data-aos-delay="300">
-<div class="flex flex-col">
-    <Icon name="ph:number-circle-one" class="text-[5rem]"/>
-    <strong class="text-2xl">Schedule at your convenience</strong>
-    <p class="process-description w-1/2">Use our user-friendly online booking system to choose a date and time that works best for you. Just fill out a brief form with your details and the specifics of your solar panel system, and we'll handle the rest. Booking online is quick, easy, and available 24/7.</p>
-    </div>
-    <img src="/solar_sustain_booking.webp" height="500px" width="600px" class="rounded-lg">
-</div>
-
-<!-- Process Card -->
-<div class="process-card grid grid-cols-2 p-32  items-center space-y-6" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="400">
-    <img src="/solar_sustain_3.webp" height="500px" width="600px" class="rounded-lg">
-    <div class="flex flex-col place-items-end  ">
-        
-        <div class="flex flex-col  w-1/2 ">
-            <Icon name="ph:number-circle-two" class="text-[5rem]"/>
-            <strong class="text-2xl">Expert Care On-Site</strong>
-            <p class="process-description " >Our professional team will visit your property to conduct a thorough assessment of your solar panel system. We'll identify any issues and perform necessary cleaning and maintenance tasks using the latest techniques and equipment. Our goal is to ensure your panels are operating at their peak efficiency.</p>
+  <section id="process" class="dark:bg-black pt-16 pb-24 overflow-hidden">
+    <div class="process-container space-y-16">
+      <h2 class="text-4xl text-center">Our Process</h2>
+      <p class="text-center text-xl w-1/2 m-auto">
+        Our process is designed to be simple, convenient, and transparent. We take the
+        hassle out of solar panel maintenance so you can enjoy the benefits of your system
+        without the stress. Here's how it works:
+      </p>
+      <!-- Process Card -->
+      <div
+        class="process-card grid grid-cols-2 p-32 items-center space-y-6"
+        data-aos="fade-up"
+        data-aos-duration="1500"
+        data-aos-delay="300"
+      >
+        <div class="flex flex-col">
+          <Icon name="ph:number-circle-one" class="text-[5rem]" />
+          <strong class="text-2xl">Schedule at your convenience</strong>
+          <p class="process-description w-1/2">
+            Use our user-friendly online booking system to choose a date and time that
+            works best for you. Just fill out a brief form with your details and the
+            specifics of your solar panel system, and we'll handle the rest. Booking
+            online is quick, easy, and available 24/7.
+          </p>
         </div>
+        <img
+          src="/solar_sustain_booking.webp"
+          height="500px"
+          width="600px"
+          class="rounded-lg"
+        />
+      </div>
+
+      <!-- Process Card -->
+      <div
+        class="process-card grid grid-cols-2 p-32 items-center space-y-6"
+        data-aos="fade-up"
+        data-aos-duration="1500"
+        data-aos-delay="400"
+      >
+        <img
+          src="/solar_sustain_3.webp"
+          height="500px"
+          width="600px"
+          class="rounded-lg"
+        />
+        <div class="flex flex-col place-items-end">
+          <div class="flex flex-col w-1/2">
+            <Icon name="ph:number-circle-two" class="text-[5rem]" />
+            <strong class="text-2xl">Expert Care On-Site</strong>
+            <p class="process-description">
+              Our professional team will visit your property to conduct a thorough
+              assessment of your solar panel system. We'll identify any issues and perform
+              necessary cleaning and maintenance tasks using the latest techniques and
+              equipment. Our goal is to ensure your panels are operating at their peak
+              efficiency.
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- Process Card -->
+      <div
+        class="process-card grid grid-cols-2 p-32 items-center space-y-6"
+        data-aos="fade-up"
+        data-aos-duration="1500"
+        data-aos-delay="500"
+      >
+        <div class="flex flex-col">
+          <Icon name="ph:number-circle-three" class="text-[5rem]" />
+          <strong class="text-2xl">Transparent and convenient Payment</strong>
+          <p class="process-description w-1/2">
+            Once the maintenance is complete, you'll receive an itemized bill detailing
+            all services rendered. You can review and pay your bill online through our
+            secure payment platform. It's straightforward, transparent, and hassle-free,
+            so you can get back to enjoying the benefits of your solar energy system with
+            minimal disruption..
+          </p>
+        </div>
+        <img
+          src="/solalr_sustain_payment.webp"
+          height="500px"
+          width="600px"
+          class="rounded-lg"
+        />
+      </div>
     </div>
-</div>
-<!-- Process Card -->
-<div class="process-card grid grid-cols-2 p-32 items-center space-y-6" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="500">
-
-    <div class="flex flex-col">
-        <Icon name="ph:number-circle-three" class="text-[5rem]"/>
-        <strong class="text-2xl">Transparent and convenient Payment</strong>
-        <p class="process-description w-1/2">Once the maintenance is complete, you'll receive an itemized bill detailing all services rendered. You can review and pay your bill online through our secure payment platform. It's straightforward, transparent, and hassle-free, so you can get back to enjoying the benefits of your solar energy system with minimal disruption..</p>
-    </div>
-    <img src="/solalr_sustain_payment.webp" height="500px" width="600px" class="rounded-lg" >
-</div>
-
-</div>
-
-    </section>
+  </section>
 </template>
