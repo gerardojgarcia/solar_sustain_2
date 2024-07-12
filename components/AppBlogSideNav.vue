ac909638ab345155535336cd8d2eb9b58d1e34ef
Gerardo J Garcia <gerardojgarcia@pm.me>
2024-07-12 10:44:56 -0700

    new blog page

--- components/AppBlogSideNav.vue
+++ components/AppBlogSideNav.vue
@@ -1,68 +1,77 @@
 <script setup>
 const pages = ref([
-  {
-    id: 1,
-    title: "Why Hire Professional Solar Panel Cleaners?",
-    date: "05-19-2024",
-    description:
-      "Explore the top reasons to choose professional services for solar panel cleaning.",
-    link: "/blog/why-hire-professional-solar-panel-cleaners",
-  },
-  {
-    id: 2,
-    title: "How Professional Solar Panel Cleaning Works",
-    date: "05-19-2024",
-    description:
-      "A detailed look at the steps and techniques used by professional cleaners.",
-    link: "/blog/how-professional-solar-panel-cleaning-works",
-  },
-  {
-    id: 3,
-    title: "Cost-Effectiveness of Professional Solar Panel Services",
-    date: "05-19-2024",
-    description:
-      "Analysis of the financial benefits of using professional cleaning services.",
-    link: "/blog/cost-effectiveness-professional-solar-panel-services",
-  },
-  {
-    id: 4,
-    title: "Top 10 Solar Panel Cleaning Companies in San Bernardino",
-    date: "05-19-2024",
-    description:
-      "Analysis of the financial benefits of using professional cleaning services.",
-    link: "/blog/top-10-professional-solar-panel-cleaning-companies",
-  },
-  {
-    id: 5,
-    title: "What to Expect from a Professional Solar Panel Cleaning Service",
-    description: "An overview of the typical service features and processes.",
-    link: "/blog/what-to-expect-professional-solar-panel-cleaning-service",
-  },
-  {
-    id: 6,
-    title: "DIY vs Professional Solar Panel Cleaning Services",
-    description: "An overview of the typical service features and processes.",
-    link: "/blog/professional-vs-diy-solar-panel-cleaning",
-  },
+    {
+        id: 1,
+        title: "Why Hire Professional Solar Panel Cleaners?",
+        date: "05-19-2024",
+        description:
+            "Explore the top reasons to choose professional services for solar panel cleaning.",
+        link: "/blog/why-hire-professional-solar-panel-cleaners",
+    },
+    {
+        id: 2,
+        title: "How Professional Solar Panel Cleaning Works",
+        date: "05-19-2024",
+        description:
+            "A detailed look at the steps and techniques used by professional cleaners.",
+        link: "/blog/how-professional-solar-panel-cleaning-works",
+    },
+    {
+        id: 3,
+        title: "Cost-Effectiveness of Professional Solar Panel Services",
+        date: "05-19-2024",
+        description:
+            "Analysis of the financial benefits of using professional cleaning services.",
+        link: "/blog/cost-effectiveness-professional-solar-panel-services",
+    },
+    {
+        id: 4,
+        title: "Top 10 Solar Panel Cleaning Companies in San Bernardino",
+        date: "05-19-2024",
+        description:
+            "Analysis of the financial benefits of using professional cleaning services.",
+        link: "/blog/top-10-professional-solar-panel-cleaning-companies",
+    },
+    {
+        id: 5,
+        title: "What to Expect from a Professional Solar Panel Cleaning Service",
+        description:
+            "An overview of the typical service features and processes.",
+        link: "/blog/what-to-expect-professional-solar-panel-cleaning-service",
+    },
+    {
+        id: 6,
+        title: "DIY vs Professional Solar Panel Cleaning Services",
+        description:
+            "An overview of the typical service features and processes.",
+        link: "/blog/professional-vs-diy-solar-panel-cleaning",
+    },
+    {
+        id: 7,
+        title: "How Often Should You Hire Professionals for Solar Panel Maintenance?",
+        description:
+            "Guidelines on the frequency of professional maintenance services.",
+        link: "/blog/how-often-hire-professionals-solar-panel-maintenance",
+    },
 ]);
 </script>
 <template>
-  <div>
-    <nav class="side-nav flex flex-col">
-      <NuxtLink to="./blog-articles">back to articles</NuxtLink>
-      <NuxtLink
-        :to="page.link"
-        class="hover:text-blue-400"
-        v-for="page in pages"
-        :key="page.id"
-        >{{ page.title }}
-      </NuxtLink>
-    </nav>
-  </div>
+    <div>
+        <nav class="side-nav flex flex-col">
+            <NuxtLink to="./blog-articles">back to articles</NuxtLink>
+            <NuxtLink
+                :to="page.link"
+                class="hover:text-blue-400"
+                v-for="page in pages"
+                :key="page.id"
+                >{{ page.title }}
+            </NuxtLink>
+        </nav>
+    </div>
 </template>
 <style scoped>
 .router-link-active {
-  font-weight: bold;
-  color: #2563eb;
+    font-weight: bold;
+    color: #2563eb;
 }
 </style>
--- components/BlogArticles.vue
+++ components/BlogArticles.vue
@@ -1,88 +1,99 @@
 <script setup lang="ts">
 const articles = ref([
-  {
-    id: 1,
-    title: "Why Hire Professional Solar Panel Cleaners?",
-    date: "05-19-2024",
-    description:
-      "Explore the top reasons to choose professional services for solar panel cleaning.",
-    link: "/blog/why-hire-professional-solar-panel-cleaners",
-    image: "/solar_sustain_1.webp",
-  },
-  {
-    id: 2,
-    title: "How Professional Solar Panel Cleaning Works",
-    date: "05-19-2024",
-    description:
-      "A detailed look at the steps and techniques used by professional cleaners.",
-    link: "/blog/how-professional-solar-panel-cleaning-works",
-    image: "/solar_sustain_2.webp",
-  },
-  {
-    id: 3,
-    title: "Cost-Effectiveness of Professional Solar Panel Services",
-    date: "05-19-2024",
-    description:
-      "Analysis of the financial benefits of using professional cleaning services.",
-    link: "/blog/cost-effectiveness-professional-solar-panel-services",
-    image: "/solar_sustain_3.webp",
-  },
-  {
-    id: 4,
-    title: "Top 10 Solar Panel Cleaning Companies in San Bernardino",
-    date: "05-19-2024",
-    description:
-      "Analysis of the financial benefits of using professional cleaning services.",
-    link: "/blog/top-10-professional-solar-panel-cleaning-companies",
-    image: "/solar_sustain_2.webp",
-  },
-  {
-    id: 5,
-    title: "What to Expect from a Professional Solar Panel Cleaning Service",
-    date: "05-19-2024",
-    description: "An overview of the typical service features and processes.",
-    link: "/blog/what-to-expect-professional-solar-panel-cleaning-service",
-    image: "/solar_sustain_3.webp",
-  },
-  {
-    id: 6,
-    title: "DIY vs Professional Solar Panel Cleaning Services",
-    date: "05-19-2024",
-    description: "An overview of the typical service features and processes.",
-    link: "/blog/professional-vs-diy-solar-panel-cleaning",
-    image: "/solar_sustain_2.webp",
-  },
+    {
+        id: 1,
+        title: "Why Hire Professional Solar Panel Cleaners?",
+        date: "05-19-2024",
+        description:
+            "Explore the top reasons to choose professional services for solar panel cleaning.",
+        link: "/blog/why-hire-professional-solar-panel-cleaners",
+        image: "/solar_sustain_1.webp",
+    },
+    {
+        id: 2,
+        title: "How Professional Solar Panel Cleaning Works",
+        date: "05-19-2024",
+        description:
+            "A detailed look at the steps and techniques used by professional cleaners.",
+        link: "/blog/how-professional-solar-panel-cleaning-works",
+        image: "/solar_sustain_2.webp",
+    },
+    {
+        id: 3,
+        title: "Cost-Effectiveness of Professional Solar Panel Services",
+        date: "05-19-2024",
+        description:
+            "Analysis of the financial benefits of using professional cleaning services.",
+        link: "/blog/cost-effectiveness-professional-solar-panel-services",
+        image: "/solar_sustain_3.webp",
+    },
+    {
+        id: 4,
+        title: "Top 10 Solar Panel Cleaning Companies in San Bernardino",
+        date: "05-19-2024",
+        description:
+            "Analysis of the financial benefits of using professional cleaning services.",
+        link: "/blog/top-10-professional-solar-panel-cleaning-companies",
+        image: "/solar_sustain_2.webp",
+    },
+    {
+        id: 5,
+        title: "What to Expect from a Professional Solar Panel Cleaning Service",
+        date: "05-19-2024",
+        description:
+            "An overview of the typical service features and processes.",
+        link: "/blog/what-to-expect-professional-solar-panel-cleaning-service",
+        image: "/solar_sustain_3.webp",
+    },
+    {
+        id: 6,
+        title: "DIY vs Professional Solar Panel Cleaning Services",
+        date: "05-19-2024",
+        description:
+            "An overview of the typical service features and processes.",
+        link: "/blog/professional-vs-diy-solar-panel-cleaning",
+        image: "/solar_sustain_2.webp",
+    },
+    {
+        id: 7,
+        title: "How Often Should You Hire Professionals for Solar Panel Maintenance?",
+        date: "07-11-2024",
+        description:
+            "Guidelines on the frequency of professional maintenance services.",
+        link: "/blog/how-often-hire-professionals-solar-panel-maintenance",
+        image: "/solar_sustain_2.webp",
+    },
 ]);
 </script>
 
 <template>
-  <div class="w-[80vw]">
-    <div
-      class="blog-article-container grid grid-cols-2 w-full place-items-center p-16 gap-10"
-    >
-      <!--blog Article -->
-      <div
-        class="blog-article-card w-96 flex flex-col p-6 rounded-md mt-6 border-2 hover:shadow-xl border-yellow-300"
-        v-for="article in articles"
-        :key="article.id"
-      >
-        <img
-          :src="article.image"
-          alt="Image of solar panel cleaners at work"
-          width="400px"
-        />
-
-        <strong class="title mt-8 text-xl">{{ article.title }}</strong>
-        <p class="date my-2">{{ article.date }}</p>
-        <p class="description mb-8 font-bold text-gray-300">
-          {{ article.description }}
-        </p>
-        <NuxtLink
-          :to="article.link"
-          class="py-4 self-end border-2 w-28 rounded-md flex justify-center hover:bg-yellow-500 duration-150 border-yellow-500"
-          >Read More</NuxtLink
+    <div class="w-[80vw]">
+        <div
+            class="blog-article-container grid grid-cols-2 w-full place-items-center p-16 gap-10"
         >
-      </div>
+            <!--blog Article -->
+            <div
+                class="blog-article-card w-96 flex flex-col p-6 rounded-md mt-6 border-2 hover:shadow-xl border-yellow-300"
+                v-for="article in articles"
+                :key="article.id"
+            >
+                <img
+                    :src="article.image"
+                    alt="Image of solar panel cleaners at work"
+                    width="400px"
+                />
+
+                <strong class="title mt-8 text-xl">{{ article.title }}</strong>
+                <p class="date my-2">{{ article.date }}</p>
+                <p class="description mb-8 font-bold text-gray-300">
+                    {{ article.description }}
+                </p>
+                <NuxtLink
+                    :to="article.link"
+                    class="py-4 self-end border-2 w-28 rounded-md flex justify-center hover:bg-yellow-500 duration-150 border-yellow-500"
+                    >Read More</NuxtLink
+                >
+            </div>
+        </div>
     </div>
-  </div>
 </template>
